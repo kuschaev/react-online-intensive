d34b536bf074dfc37f618bf2c16ed675bcf01607
Nikolai Kuschaev <november_kilo@Nikolais-Air.192.168.1.1>
2019-03-26 04:14:12 +0200

    Feed, Composer, Post parameterized thru props from App

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -2,16 +2,19 @@
 import React, { Component } from 'react';
 
 // Instruments
-import avatar from 'theme/assets/lisa';
 import Styles from './styles.m.css';
 
 export default class Composer extends Component {
     render() {
+        const {avatar, currentUserFirstName} = this.props;
+
         return (
             <section className = {Styles.composer}>
                 <img src = {avatar} />
                 <form>
-                    <textarea placeholder = {`What's up, Lisa?`}/>
+                    <textarea
+                        placeholder = {`What's up, ${currentUserFirstName}?`}
+                    />
                     <input type = 'submit' value = 'Post' />
                 </form>
             </section>
--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -11,11 +11,15 @@ import Styles from './styles.m.css';
 
 export default class Feed extends Component {
     render() {
+        const {avatar, currentUserFirstName} = this.props;
         return (
             <section className = {Styles.feed}>
                 <StatusBar />
-                <Composer />
-                <Post />
+                <Composer
+                    avatar = {avatar}
+                    currentUserFirstName = {currentUserFirstName}
+                />
+                <Post {...this.props} />
             </section>
         );
     }
