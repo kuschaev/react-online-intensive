78820e9372b7c6e663faac84b9ccab0249dc9fd6
Nikolai Kuschaev <november_kilo@Nikolais-Air.192.168.1.1>
2019-04-03 01:39:05 +0300

    Lesson 5 state

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 
 // Components
+import Spinner from 'components/Spinner';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
@@ -10,12 +11,30 @@ import Post from 'components/Post';
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    constructor() {
+        super();
+        const now = Date.now();
+        this.state = {
+            posts: [
+                {id: '123', comment: 'Hello!', created: now},
+                {id: '456', comment: 'Hi!', created: now - 100},
+                {id: '789', comment: 'Howdy! 👋', created: now - 300}
+            ]
+        };
+    }
+
     render() {
+        const {posts} = this.state;
+        const postsJSX = posts.map(post => {
+            return <Post key = {post.id} {...post}/>;
+        });
+        
         return (
             <section className = {Styles.feed}>
+                <Spinner isSpinning />
                 <StatusBar />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>
         );
     }
